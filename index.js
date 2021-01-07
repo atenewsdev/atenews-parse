f1e2f31c4a80c8efe20205f5eae75e7a5589cf8e
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-01-07 11:31:51 +0800

    add dashboard and gql

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // compatible API routes.
 
 var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
+var { default: ParseServer, ParseGraphQLServer } = require('parse-server');
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -11,7 +12,7 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
+const api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
@@ -21,6 +22,31 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
+const parseGraphQLServer = new ParseGraphQLServer(
+  parseServer,
+  {
+    graphQLPath: '/graphql',
+    playgroundPath: '/playground'
+  }
+);
+
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      serverURL: process.env.SERVER_URL,
+      appId: process.env.APP_ID,
+      masterKey: process.env.MASTER_KEY,
+      appName: "Atenews"
+    }
+  ],
+  users: [
+    {
+      user: process.env.DASHBOARD_USERNAME,
+      pass: process.env.DASHBOARD_PASSWORD
+    }
+  ]
+}, { allowInsecureHTTP: false });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -33,6 +59,13 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+
+// Mounts the GraphQL API using graphQLPath: '/graphql'
+parseGraphQLServer.applyGraphQL(app);
+// (Optional) Mounts the GraphQL Playground - do NOT use in Production
+parseGraphQLServer.applyPlayground(app);
+
+app.use('/dashboard', dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
